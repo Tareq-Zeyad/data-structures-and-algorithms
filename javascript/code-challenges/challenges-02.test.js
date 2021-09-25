4923fd973a005a7433ee64b0ff71996ea7cdb458
Tariq Zeyad Sharaia <tariq.z.sharaia@gmail.com>
2021-09-25 16:33:11 +0300

    CC-map-stretch solved

--- javascript/code-challenges/challenges-02.test.js
+++ javascript/code-challenges/challenges-02.test.js
@@ -104,6 +104,10 @@ For example: charCode(['h','i']) returns [104, 105].
 
 const charCode = (arr) => {
   // Solution code here...
+  let newArr= arr.map(item => {
+    return item.charCodeAt(0);
+  })
+  return newArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -118,6 +122,20 @@ For example: evenOdd([1,2,3]) returns ['odd','even','odd'].
 
 const evenOdd = (arr) => {
   // Solution code here...
+  let numArr = arr.map(item => {
+    if(isNaN(item) === false){
+      if(item % 2 === 0){
+        return item='even';
+      }
+      else{
+          return item = 'odd';
+        }
+
+    }else{
+      return item = 'N/A';
+    }
+  })
+  return numArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -164,6 +182,10 @@ const snorlaxAbilities = {
 
 const extractAbilities = (arr) => {
   // Solution code here...
+  let tariq = arr.map(item => {
+    return item.ability.name;
+  })
+  return tariq;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -211,6 +233,15 @@ const snorlaxStats = {
 
 const extractStats = (arr) => {
   // Solution code here...
+  let newArr =[];
+  newArr = arr.map(item => {
+    let total = `${item.effort+item.baseStat}`;
+    return{
+      name:`${item.stat.name}`,
+      total: Number(total)
+    };
+  });
+return newArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -275,14 +306,14 @@ describe('Testing challenge 6', () => {
   });
 });
 
-xdescribe('Testing challenge 7', () => {
+describe('Testing challenge 7', () => {
   test('It should return an array containing the character code for each letter', () => {
     expect(charCode(['C', 'o', 'd', 'e', '3', '0', '1'])).toStrictEqual([ 67, 111, 100, 101, 51, 48, 49 ]);
     expect(charCode(['C', 'o', 'd', 'e', '3', '0', '1']).length).toStrictEqual(7);
   });
 });
 
-xdescribe('Testing challenge 8', () => {
+describe('Testing challenge 8', () => {
   test('It should return an array containing the keys from an object', () => {
     expect(evenOdd([5, 8, 2, 6, 9, 13, 542, 541])).toStrictEqual([ 'odd', 'even', 'even', 'even', 'odd', 'odd', 'even', 'odd' ]);
     expect(evenOdd([5, 8, 2, 6, 9, 13, 542, 541]).length).toStrictEqual(8);
@@ -304,14 +335,14 @@ xdescribe('Testing challenge 8', () => {
   });
 });
 
-xdescribe('Testing challenge 9', () => {
+describe('Testing challenge 9', () => {
   test('It should return an array containing only the ability names', () => {
     expect(extractAbilities(snorlaxAbilities.abilities)).toStrictEqual(['gluttony', 'cute charm', 'immunity']);
     expect(extractAbilities(snorlaxAbilities.abilities).length).toStrictEqual(3);
   });
 });
 
-xdescribe('Testing challenge 10', () => {
+describe('Testing challenge 10', () => {
   test('It should return an array containing objects with name and total values', () => {
     expect(extractStats(snorlaxStats.stats)).toStrictEqual([
       { name: 'speed', total: 35, },
