5956d8f1848db02943dd31b1c9de0d7d9e8a8265
Tariq Zeyad Sharaia <tariq.z.sharaia@gmail.com>
2021-11-08 22:12:05 +0200

    k-th lab

---
+++ python/data_structures/data_structures/linked_list/linked_list.py
@@ -0,0 +1,174 @@
+class Node:
+    """
+    Node class has the properties for the value in the Node, & a pointer to the next node.
+    """
+    counter = 0
+
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    """
+    Class for Linked List for creating Nodes
+    """
+
+    def __init__(self):
+        self.head = None
+
+    def insert(self, addValue):
+        node = Node(addValue)
+        if self.head is None:
+            self.head - node
+
+        else:
+            current = self.head
+            while current.next != None:
+                current = current.next
+
+                LinkedList.counter += 1
+                current.next = node
+                return "Value is added"
+
+
+@classmethod
+def countering(cls):
+    return cls.counter
+
+
+def __str__(self):
+    output = ""
+    if self.head is None:
+        output += None
+    else:
+        current = self.head
+        while(current):
+
+            output += ("{"+str(current.value)+"}"+" -> ")
+            current = current.next
+        output += "None"
+        return output
+
+
+def includes(self, valueSearched):
+    """
+    method to search for a specifc value
+    """
+    current = self.head
+    if self.head != None:
+        while current.next != None:
+            if current.value == valueSearched:
+                print("true")
+                return True
+
+            current = current.next
+        print("false")
+        return False
+    else:
+        print("Empty")
+        return ("Empty")
+
+
+def append(self, value):
+    """
+    Add a value at the end of a linked list, if no value exists, it will be the head of the list.
+    """
+    current = self.head
+
+    if(current != None):
+        while(True):
+            if current.next != None:
+                current = current._next
+            else:
+                current.next = Node(value)
+                break
+    else:
+        self.head = Node(value)
+
+
+def insert_before(self, before_value, value):
+    """
+    Insert a given value before a specificed itme in the linked list.
+    """
+    current = self.head
+
+    if(current != None):
+        while(True):
+            if current._next == None:
+                if current.data == before_value:
+                    self.head = Node(value, current)
+                    break
+                else:
+                    break
+            elif current._next.data == before_value:
+                current._next = Node(value, current._next)
+                break
+            else:
+                current = current._next
+    else:
+        self.head = Node(value)
+
+
+def insert_after(self, after_value, value):
+    """
+    Insert a given value after a specificed item in the linked list.
+    """
+    current = self.head
+
+    if(current != None):
+        while(True):
+            if current._next == None:
+                if current.data == after_value:
+                    current._next = Node(value, current._next)
+                    break
+                else:
+                    break
+            elif current.data == after_value:
+                current._next = Node(value, current._next)
+                break
+            else:
+                current = current._next
+
+
+def kthFromEnd(self, input):
+    current = self.head
+
+    if input == 1:
+        return current.data
+    elif(current != None and current._next != None and input > 0):
+        count = 1
+        second_pointer = [None]
+
+        while(True):
+            if count == input:
+                second_pointer[0] = current
+            elif count - input == input:
+                second_pointer[0] = second_pointer[0]._next
+
+            if current._next == None:
+                if(count < input):
+                    return None
+                else:
+                    return second_pointer[0].data
+            else:
+                current = current._next
+                count += 1
+
+    elif (current != None and current._next != None and input == 0):
+        while(True):
+            if current._next == None:
+                return current.data
+            else:
+                current = current._next
+    else:
+        return None
+
+
+if __name__ == '__main__':
+
+    ll = LinkedList()
+
+    ll.append(7)
+    ll.append(1)
+    ll.append(2)
---
+++ python/data_structures/tests/test_linked_list.py
@@ -0,0 +1,119 @@
+from linked_list import __version__
+from linked_list.linked_list import Node,  LinkedList
+import pytest
+
+
+def test_version():
+    assert __version__ == '0.1.0'
+
+
+def test_empty_linked_list():
+    ll = LinkedList()
+    expected = None
+    actual = ll.head
+    assert expected == actual
+
+
+def test_inserting_one_value():
+    ll = LinkedList()
+    ll.insert(3)
+    expected = "value added"
+    actual = ll.insert(3)
+    assert expected == actual
+
+
+def test_head_pointing_to_next_node():
+    node = Node(10)
+    expected = 10
+    actual = node.value
+    assert actual == expected
+
+
+def test_multi_values_added(ll):
+    expected = 3
+    actual = ll.countering()
+    assert expected == actual
+
+
+def test_all_values_in_LL(ll):
+    expected = 'head -> [10] -> [30] -> [500] -> None'
+    actual = ll.__str__()
+    assert expected == actual
+
+
+def test_value_found(ll):
+
+    expected = False
+    actual = ll.includes("Issa")
+    assert expected == actual
+
+
+def test_value_not_found(ll):
+
+    expected = True
+    actual = ll.includes(10)
+    assert expected == actual
+
+
+def test_insert_before(ll):
+    ll.insert_before(30, 40)
+    expected = 'head -> [10] -> [40] -> [30] -> [500] -> None'
+    actual = ll.__str__()
+    assert expected == actual
+
+
+def test_insert_after(ll):
+    ll.insert_after(30, 40)
+    expected = 'head -> [10] -> [30] -> [40] -> [500] -> None'
+    actual = ll.__str__()
+    assert expected == actual
+
+
+def test_greater_than_the_length(ll):
+
+    expected = 'k is greater than the length of the linked list'
+    actual = ll.kthFromEnd(10)
+    assert expected == actual
+
+
+def test_k_and_the_length_of_the_list_are_the_same(ll):
+    expected = 10
+    actual = ll.kthFromEnd(3)
+    assert expected == actual
+
+
+def test_k_is_not_a_positive_integer(ll):
+    expected = "Please input positive numbers"
+    actual = ll.kthFromEnd(-3)
+    assert expected == actual
+
+
+def test__linked_list_is_of_a_size_1():
+    ll = LinkedList()
+    ll.append(3)
+    expected = 3
+    actual = ll.kthFromEnd(0)
+    assert expected == actual
+
+
+def test__Happy_Pat_K_is_not_in_middle():
+    ll = LinkedList()
+    ll.append(1)
+    ll.append(4)
+    ll.append(2)
+    ll.append(15)
+    ll.append(6)
+
+    assert ll.kthFromEnd(1) == 20
+    assert ll.kthFromEnd(2) == 4
+    assert ll.kthFromEnd(3) == 8
+
+
+@pytest.fixture
+def ll():
+    ll = LinkedList()
+    ll.append(10)
+    ll.append(30)
+    ll.append(500)
+
+    return ll
