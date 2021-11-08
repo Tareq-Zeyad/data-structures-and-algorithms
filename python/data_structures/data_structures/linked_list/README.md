00becbfd987bfee38540fe5119e58376248ef1d3
Tariq Zeyad Sharaia <tariq.z.sharaia@gmail.com>
2021-11-08 19:53:54 +0200

    added README

---
+++ python/data_structures/data_structures/linked_list/README.md
@@ -0,0 +1,33 @@
+
+# code-challenge 5
+# Singly Linked List
+
+
+### Create Node class and Linked list class to add new node that has a value and a reference to next node.
+<br>
+
+
+
+## Challenge
+## its a challenge to make Linked list and start adding nodes that has a value and points to the next node and last node points to None
+
+## Approach & Efficiency
+## The Efficiency of Big O is O(n) linear.
+
+## API
+## We have 3 methods:
+- ### **append method** : that used to add new node to the linked lists
+- ### **search method**: that used to search for specific values in the linked list
+- ### **str method**: to give a presenting way to the user when he prints the object after adding values for example
+- ### **counter method** : to count how many times we added a value
+
+## Task lists
+- ### [x] Can successfully instantiate an empty linked list
+
+- ### [x] Can properly insert into the linked list
+
+- ### [x] The head property will properly point to the first node in the linked list
+- ### [x] Can properly insert multiple nodes into the linked list
+- ### [x] Will return true when finding a value within the linked list that exists
+- ### [x] Will return false when searching for a value in the linked list that does not exist
+- ### [x] Can properly return a collection of all the values that exist in the linked list
---
+++ python/data_structures/tests/test_linked_list.py
@@ -0,0 +1,79 @@
+from linked_list import __version__
+from linked_list.linked_list import Node,  LinkedList
+import pytest
+
+
+def test_version():
+    assert __version__ == '0.1.0'
+
+
+def test_empty_linked_list():
+    ll = LinkedList()
+    expected = None
+    actual = ll.head
+    assert expected == actual
+
+
+def test_inserting_one_value():
+    ll = LinkedList()
+    ll.insert(3)
+    expected = "value added"
+    actual = ll.insert(3)
+    assert expected == actual
+
+
+def test_head_pointing_to_next_node():
+    node = Node(10)
+    expected = 10
+    actual = node.value
+    assert actual == expected
+
+
+def test_multi_values_added(ll):
+    expected = 3
+    actual = ll.countering()
+    assert expected == actual
+
+
+def test_all_values_in_LL(ll):
+    expected = 'head -> [10] -> [30] -> [500] -> None'
+    actual = ll.__str__()
+    assert expected == actual
+
+
+def test_value_found(ll):
+
+    expected = False
+    actual = ll.includes("Issa")
+    assert expected == actual
+
+
+def test_value_not_found(ll):
+
+    expected = True
+    actual = ll.includes(10)
+    assert expected == actual
+
+
+def test_insert_before(ll):
+    ll.insert_before(30, 40)
+    expected = 'head -> [10] -> [40] -> [30] -> [500] -> None'
+    actual = ll.__str__()
+    assert expected == actual
+
+
+def test_insert_after(ll):
+    ll.insert_after(30, 40)
+    expected = 'head -> [10] -> [30] -> [40] -> [500] -> None'
+    actual = ll.__str__()
+    assert expected == actual
+
+
+@pytest.fixture
+def ll():
+    ll = LinkedList()
+    ll.append(11)
+    ll.append(20)
+    ll.append(400)
+
+    return ll
