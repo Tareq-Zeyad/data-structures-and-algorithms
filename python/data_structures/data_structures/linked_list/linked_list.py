8272ed3baf98c715bf65e245cdf09101757abcea
Tariq Zeyad Sharaia <tariq.z.sharaia@gmail.com>
2021-11-09 23:19:02 +0200

    whiteboard 07

--- python/data_structures/data_structures/linked_list/linked_list.py
+++ python/data_structures/data_structures/linked_list/linked_list.py
@@ -1,176 +1,191 @@
 class Node:
     """
-    Node class has the properties for the value in the Node, & a pointer to the next node.
+    A class represting a Node.
+    Data and other attributes:
+    data: Any type of valid data
+    _next: Next Node
     """
-    counter = 0
 
-    def __init__(self, value):
-        self.value = value
-        self.next = None
+    def __init__(self, data, _next=None):
+        self.data = data
+        self._next = _next
+        pass
 
 
 class LinkedList:
     """
-    Class for Linked List for creating Nodes
+    A class for creating instances of a Linked List.
+
+    Data and other attributes defined here:
+
+    head: Node | None
+    Methods defined here
+    insert(value: any)
+    contains(value: any) -> bool
     """
 
     def __init__(self):
         self.head = None
 
-    def insert(self, addValue):
-        node = Node(addValue)
-        if self.head is None:
-            self.head - node
+    def insert(self, value):
+        """"
+        Insert creates a Node with the value that was passed and adds
+        it to the head of the linked list shifting all other values down
+        arguments:
+        value : any
+        returns: None
+        """
+        self.head = Node(value, self.head)
 
-        else:
-            current = self.head
-            while current.next != None:
-                current = current.next
-
-                LinkedList.counter += 1
-                current.next = node
-                return "Value is added"
-
-
-@classmethod
-def countering(cls):
-    return cls.counter
-
-
-def __str__(self):
-    output = ""
-    if self.head is None:
-        output += None
-    else:
+    def includes(self, val):
+        """"
+        Check weather a value exists in a list instance
+        arguments:
+        value : any
+        returns: True/False
+        """
         current = self.head
-        while(current):
 
-            output += ("{"+str(current.value)+"}"+" -> ")
-            current = current.next
-        output += "None"
-        return output
-
-
-def includes(self, valueSearched):
-    """
-    method to search for a specifc value
-    """
-    current = self.head
-    if self.head != None:
-        while current.next != None:
-            if current.value == valueSearched:
-                print("true")
-                return True
-
-            current = current.next
-        print("false")
-        return False
-    else:
-        print("Empty")
-        return ("Empty")
-
-
-def append(self, value):
-    """
-    Add a value at the end of a linked list, if no value exists, it will be the head of the list.
-    """
-    current = self.head
-
-    if(current != None):
-        while(True):
-            if current.next != None:
-                current = current._next
-            else:
-                current.next = Node(value)
-                break
-    else:
-        self.head = Node(value)
-
-
-def insert_before(self, before_value, value):
-    """
-    Insert a given value before a specificed itme in the linked list.
-    """
-    current = self.head
-
-    if(current != None):
-        while(True):
-            if current._next == None:
-                if current.data == before_value:
-                    self.head = Node(value, current)
-                    break
+        if(current != None):
+            while(True):
+                if current.data == val:
+                    return True
+                elif current.data != val and current._next == None:
+                    return False
                 else:
-                    break
-            elif current._next.data == before_value:
-                current._next = Node(value, current._next)
-                break
+                    current = current._next
+
+    def __str__(self):
+        """"
+        Returns a string containing all elements that exist within the list, if none it will return NULL only.
+        Example: 
+        "{ 29 } -> { 9 } -> { 5 } -> { 0 } -> NULL"
+        arguments:
+        None
+        returns: String
+        """
+        current = self.head
+        full_string = ''
+        while(current != None):
+            if(current._next == None):
+                full_string += "{ "+str(current.data)+" } -> NULL"
+                return full_string
             else:
+                full_string += "{ "+str(current.data)+" } -> "
                 current = current._next
-    else:
-        self.head = Node(value)
 
+        return "NULL"
 
-def insert_after(self, after_value, value):
-    """
-    Insert a given value after a specificed item in the linked list.
-    """
-    current = self.head
+    def append(self, value):
+        """
+        Add a value to the end of the linked list, if none exists the value will be the head of the list.
+        """
+        current = self.head
 
-    if(current != None):
-        while(True):
-            if current._next == None:
-                if current.data == after_value:
+        if(current != None):
+            while(True):
+                if current._next != None:
+                    current = current._next
+                else:
+                    current._next = Node(value)
+                    break
+        else:
+            self.head = Node(value)
+
+    def insert_before(self, before_value, value):
+        """
+        Insert a given value before a specificed value in the linked list.
+        """
+        current = self.head
+
+        if(current != None):
+            while(True):
+                if current._next == None:
+                    if current.data == before_value:
+                        self.head = Node(value, current)
+                        break
+                    else:
+                        break
+                elif current._next.data == before_value:
                     current._next = Node(value, current._next)
                     break
                 else:
+                    current = current._next
+        else:
+            self.head = Node(value)
+
+    def insert_after(self, after_value, value):
+        """
+        Insert a given value after a specificed value in the linked list.
+        """
+        current = self.head
+
+        if(current != None):
+            while(True):
+                if current._next == None:
+                    if current.data == after_value:
+                        current._next = Node(value, current._next)
+                        break
+                    else:
+                        break
+                elif current.data == after_value:
+                    current._next = Node(value, current._next)
                     break
-            elif current.data == after_value:
-                current._next = Node(value, current._next)
-                break
-            else:
-                current = current._next
-
-
-
-def kthFromEnd(self, input):
-    current = self.head
-
-    if input == 1:
-        return current.data
-    elif(current != None and current._next != None and input > 0):
-        count = 1
-        second_pointer = [None]
-
-        while(True):
-            if count == input:
-                second_pointer[0] = current
-            elif count - input == input:
-                second_pointer[0] = second_pointer[0]._next
-
-            if current._next == None:
-                if(count < input):
-                    return None
                 else:
-                    return second_pointer[0].data
-            else:
-                current = current._next
-                count += 1
+                    current = current._next
 
-    elif (current != None and current._next != None and input == 0):
-        while(True):
-            if current._next == None:
-                return current.data
-            else:
-                current = current._next
-    else:
-        return None
+    def kthFromEnd(self, input):
+        current = self.head
 
+        if input == 1:
+            return current.data
+        elif(current != None and current._next != None and input > 0):
+            count = 1
+            second_pointer = [None]
 
+            while(True):
+                if count == input:
+                    second_pointer[0] = current
+                elif count - input == input:
+                    second_pointer[0] = second_pointer[0]._next
 
-if __name__ == '__main__':
+                if current._next == None:
+                    if(count < input):
+                        return None
+                    else:
+                        return second_pointer[0].data
+                else:
+                    current = current._next
+                    count += 1
 
-    ll = LinkedList()
+        elif (current != None and current._next != None and input == 0):
+            while(True):
+                if current._next == None:
+                    return current.data
+                else:
+                    current = current._next
+        else:
+            return None
 
-    ll.append(7)
-    ll.append(1)
-    ll.append(2)
+    @staticmethod
+    def zipLists(first_list, second_list):
+        current = first_list.head
+
+        if(current != None and second_list.head != None):
+            newList = LinkedList()
+            counter = 1
+            while(True):
+                if current._next == None and counter == 2:
+                    newList.append(current.data)
+                    return newList
+
+                elif current._next == None and counter == 1:
+                    newList.append(current.data)
+                    counter = 2
+                    current = second_list.head
+                else:
+                    newList.append(current.data)
+                    current = current._next
+        else:
+            raise Exception(
+                "One of the provided arguments does not meet the required type of data/ either linked list is empty...")
